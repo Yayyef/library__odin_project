57e8cf125dcb002165995fd106f8073b9b202943
XChassang <chassang.xavier@gmail.com>
2021-12-03 17:20:59 +0100

    Fichier créés. Ajout du constructeur d'objet pour les films.

---
+++ script.js
@@ -0,0 +1,17 @@
+function Film(title, director, duration, seen) {
+    this.title = title,
+    this.director = director,
+    this.duration = duration,
+    this.seen = seen
+};
+
+Film.prototype.info = function() {
+    if (this.seen === false) {
+        return `${this.title}, directed by ${this.director}, ${this.duration} minutes long, not seen yet`;
+    } if (this.seen === true) {
+        return `${this.title}, directed by ${this.director}, ${this.duration} minutes long, already seen`;
+}
+};
+
+const lesOlympiades = new Film('Les Olympiades', 'Jacques Audiard', 120, true);
+const trePiani = new Film('Tre Piani', 'Nanni Morreti', 105, false);
