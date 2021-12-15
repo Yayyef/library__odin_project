ca0603e22ce520ff2f6dbe09809a3d4e69257eb0
Xavier Chassang <chassang.xavier@gmail.com>
2021-12-15 21:32:01 +0100

    Création d'un EventListener pour récupérer les données saisies par l'utilisateur et en faire un objet Film.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let myMovies = [];
-const bookContainer = document.querySelector(".book-container");
-const lesOlympiades = new Film('Les Olympiades', 'Jacques Audiard', 120, true);
-const trePiani = new Film('Tre Piani', 'Nanni Morreti', 105, false);
+const bookContainer = document.querySelector(".movies-container");
+const submitFilmButton = document.querySelector("#submitFilmButton");
+const lesOlympiades = new Film('Les Olympiades', 'Jacques Audiard', 2021, 120, true);
+const trePiani = new Film('Tre Piani', 'Nanni Morreti', 2021, 105, false);
 myMovies.push(lesOlympiades);
 myMovies.push(trePiani);
 
-
-function Film(title, director, duration, seen) {
+// Le constructeur d'objet qui sera utilisé et son prototype (pour économiser de la mémoire)
+function Film(title, director, date, duration, seen) {
     this.title = title,
     this.director = director,
+    this.date = date,
     this.duration = duration,
     this.seen = seen
 };
-
 Film.prototype.info = function() {
     if (this.seen === false) {
         return `${this.title}, directed by ${this.director}, ${this.duration} minutes long, not seen yet`;
@@ -29,15 +30,17 @@ function addMovieToMyMovies(movie) {
 // Construction des cartes films et affichage des objets contenus dans le tableau myMovies
 myMovies.forEach(movie => {
     const movieCard = document.createElement("div");
-    movieCard.classList.add(".book-card");
+    movieCard.classList.add("movie-card");
 
     const movieTitle = document.createElement("h3");
-    const movieDirector = document.createElement("p");
+    const movieDirector = document.createElement("h4");
+    const movieDate = document.createElement("p");
     const movieDuration = document.createElement("p");
     const movieSeen = document.createElement("p");
     
     movieTitle.textContent = movie.title;
     movieDirector.textContent = movie.director;
+    movieDate.textContent = movie.date;
     movieDuration.textContent = movie.duration;
     movieSeen.textContent = movie.seen;
     
@@ -47,6 +50,23 @@ myMovies.forEach(movie => {
     movieCard.appendChild(movieDirector);
     movieCard.appendChild(movieDuration);
     movieCard.appendChild(movieSeen);   
+});
+
+submitFilmButton.addEventListener("click", () => {
+    const userTitle = document.querySelector("#userTitle").value;
+    const userDirector = document.querySelector("#userDirector").value;
+    const userDate = document.querySelector("#userDate").value;
+    const userDuration = document.querySelector("#userDuration").value;
+    const userSeen = document.querySelector("#userSeen").value;
+
+    console.log(userTitle);
+    console.log(userDirector);
+    console.log(userDate);
+    console.log(userDuration);
+    console.log(userSeen);
+
+    const userFilm = new Film(userTitle, userDirector, userDate, 120, true);
+    console.log(userFilm);
 });
 
 const myObject = Object.create(Object.prototype);
