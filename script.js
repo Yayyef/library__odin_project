274b48f115c4dbcdbc9626f01bdd75484b680f0e
Xavier Chassang <chassang.xavier@gmail.com>
2021-12-16 13:59:26 +0100

    Ajout de la fonction pour plus de clareté et de réutilisabilité. Modification structure HTML pour plus de clarté.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let myMovies = [];
 const bookContainer = document.querySelector(".movies-container");
 const submitFilmButton = document.querySelector("#submitFilmButton");
-const lesOlympiades = new Film('Les Olympiades', 'Jacques Audiard', 2021, 120, true);
-const trePiani = new Film('Tre Piani', 'Nanni Morreti', 2021, 105, false);
+const lesOlympiades = new Movie('Les Olympiades', 'Jacques Audiard', 2021, 120, true);
+const trePiani = new Movie('Tre Piani', 'Nanni Morreti', 2021, 105, false);
 myMovies.push(lesOlympiades);
 myMovies.push(trePiani);
 
 // Le constructeur d'objet qui sera utilisé et son prototype (pour économiser de la mémoire)
-function Film(title, director, date, duration, seen) {
+function Movie(title, director, date, duration, seen) {
     this.title = title,
     this.director = director,
     this.date = date,
     this.duration = duration,
     this.seen = seen
 };
-Film.prototype.info = function() {
+Movie.prototype.info = function() {
     if (this.seen === false) {
         return `${this.title}, directed by ${this.director}, ${this.duration} minutes long, not seen yet`;
     } else if (this.seen === true) {
@@ -28,7 +28,7 @@ function addMovieToMyMovies(movie) {
 }
 
 // Construction des cartes films et affichage des objets contenus dans le tableau myMovies
-myMovies.forEach(movie => {
+const cardCreation = (movie) => {
     const movieCard = document.createElement("div");
     movieCard.classList.add("movie-card");
 
@@ -48,25 +48,25 @@ myMovies.forEach(movie => {
 
     movieCard.appendChild(movieTitle);
     movieCard.appendChild(movieDirector);
+    movieCard.appendChild(movieDate);
     movieCard.appendChild(movieDuration);
     movieCard.appendChild(movieSeen);   
-});
+};
 
+// Au chargement de la page, la fonction créant les fiches de film est appellée sous les films du tableau myMovies
+window.addEventListener("onload", myMovies.forEach(movie => cardCreation(movie)));
+
+// Lorsqu'on clique sur #submitFilmButton, les valeurs remplies par l'utilisateur sont stockées dans des variables qui permettent la construction d'un objet Film
 submitFilmButton.addEventListener("click", () => {
     const userTitle = document.querySelector("#userTitle").value;
     const userDirector = document.querySelector("#userDirector").value;
     const userDate = document.querySelector("#userDate").value;
     const userDuration = document.querySelector("#userDuration").value;
     const userSeen = document.querySelector("#userSeen").value;
-
-    console.log(userTitle);
-    console.log(userDirector);
-    console.log(userDate);
-    console.log(userDuration);
-    console.log(userSeen);
-
-    const userFilm = new Film(userTitle, userDirector, userDate, 120, true);
-    console.log(userFilm);
+    
+    const userFilm = new Movie(userTitle, userDirector, userDate, 120, true);
+    addMovieToMyMovies(userFilm);
+    cardCreation(userFilm);
 });
 
 const myObject = Object.create(Object.prototype);
