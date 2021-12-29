6e53f2cf619e95c73d7297d812396fe8e30a01aa
Xavier Chassang <chassang.xavier@gmail.com>
2021-12-29 11:20:33 +0100

    Ajout d'un boutton remove fonctionnel.

--- script.js
+++ script.js
@@ -13,24 +13,39 @@ function Movie(title, director, date, duration, seen) {
     this.date = date,
     this.duration = duration,
     this.seen = seen
-};
+}
 Movie.prototype.info = function() {
     if (this.seen === false) {
         return `${this.title}, directed by ${this.director}, ${this.duration} minutes long, not seen yet`;
     } else if (this.seen === true) {
         return `${this.title}, directed by ${this.director}, ${this.duration} minutes long, already seen`;
 }
-};
+}
 
 // Ajoute les films crées dans le tableau myMovies
-function addMovieToMyMovies(movie) {
-    myMovies.push(movie);
+// function addMovieToMyMovies(movie) {
+//     myMovies.push(movie);
+// }
+function displayAllMovies() {
+    removeMovieCards();
+
+    for(i = 0; i < myMovies.length; i++) {
+        cardCreation(myMovies[i], i);
+    }
+    // myMovies.forEach(movie => cardCreation(movie))
+}
+function removeMovieCards() {
+    const movieCards = Array.from(document.querySelectorAll('.movie-card'));
+    for(i = 0; i < movieCards.length; i++) {
+        movieCards[i].remove();
+    }
 }
 
 // Construction des cartes films et affichage des objets contenus dans le tableau myMovies
-const cardCreation = (movie) => {
+const cardCreation = (movie, index) => {
     const movieCard = document.createElement("div");
     movieCard.classList.add("movie-card");
+
 
     const movieTitle = document.createElement("h3");
     const movieDirector = document.createElement("h4");
@@ -38,6 +53,7 @@ const cardCreation = (movie) => {
     const movieDuration = document.createElement("p");
     const movieSeen = document.createElement("p");
     
+
     movieTitle.textContent = movie.title;
     movieDirector.textContent = movie.director;
     movieDate.textContent = movie.date;
@@ -45,6 +61,19 @@ const cardCreation = (movie) => {
     movieSeen.textContent = movie.seen;
     
     bookContainer.appendChild(movieCard);
+    
+    // Ici je défini un attribut qui me permet de trouver l'index du film à supprimer de l'array
+    movieCard.setAttribute("data-index", index);
+
+    // On peut avec le boutton retirer un élément du tableau, mais tout reste affiché et les data-index demeurent les mêmes. Il faut donc rafraichir la page à chaque fois OU BIEN retirer l'élément visé et mettre à jour les data-index.
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", (e) => {
+        myMovies.splice((e.target.parentElement.attributes["data-index"].value), 1);
+        alert(`${e.target.nextElementSibling.innerText} has been removed.`);
+        displayAllMovies();
+    });
+    movieCard.appendChild(removeButton);
 
     movieCard.appendChild(movieTitle);
     movieCard.appendChild(movieDirector);
@@ -54,7 +83,7 @@ const cardCreation = (movie) => {
 };
 
 // Au chargement de la page, la fonction créant les fiches de film est appellée sous les films du tableau myMovies
-window.addEventListener("onload", myMovies.forEach(movie => cardCreation(movie)));
+window.addEventListener("onload", displayAllMovies());
 
 // Lorsqu'on clique sur #submitFilmButton, les valeurs remplies par l'utilisateur sont stockées dans des variables qui permettent la construction d'un objet Film
 submitFilmButton.addEventListener("click", () => {
@@ -62,11 +91,16 @@ submitFilmButton.addEventListener("click", () => {
     const userDirector = document.querySelector("#userDirector").value;
     const userDate = document.querySelector("#userDate").value;
     const userDuration = document.querySelector("#userDuration").value;
-    const userSeen = document.querySelector("#userSeen").value;
+    // const userSeen = document.querySelector("#userSeen").value;
     
-    const userFilm = new Movie(userTitle, userDirector, userDate, 120, true);
-    addMovieToMyMovies(userFilm);
-    cardCreation(userFilm);
+    removeMovieCards();
+
+    const userFilm = new Movie(userTitle, userDirector, userDate, userDuration, true);
+    myMovies.push(userFilm);
+    displayAllMovies();
+
+    // cardCreation(userFilm);
 });
 
-const myObject = Object.create(Object.prototype);
+// Il faudrait que j'essaye d'utiliser object.create.
+// const myObject = Object.create(Object.prototype);
